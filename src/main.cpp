ccbc64252dfd44b8064810976cce8b35f26a6549
lilian <li4bou@gmail.com>
2025-06-16 21:17:44 +0200

    feat: better image saving

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,14 @@ int main() {
     cv::Mat image(camera->get_height(), camera->get_width(), CV_8UC3, cv::Scalar(0, 0, 0));
     scene->render(camera, image);
 
-    // Save the image
-    cv::imwrite("build/output.png", image);
+    // Get current time
+    auto now = std::chrono::system_clock::now();
+    auto time = std::chrono::system_clock::to_time_t(now);
+    std::stringstream ss;
+    ss << "images/output_" << std::put_time(std::localtime(&time), "%y%m%d_%H%M") << ".png";
+
+    // Save the image with timestamp
+    cv::imwrite(ss.str(), image);
 
     delete scene;
     delete camera;
